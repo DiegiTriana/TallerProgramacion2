e0f598916eb7645593fca5f34b5658bbac65ce7f
DiegoTriana <diegotriana2005@gmail.com>
2024-02-29 13:32:44 -0500

    cambio

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -5,8 +5,25 @@ public class Ejercicio11 {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Digite la cantidad de alumnos para el viaje");
+        int alumnos = teclado.nextInt();
         
+        if (alumnos >= 100)
+        {
+            System.out.println("El pago a la agencia es: " + alumnos*65 + "" + " \n Cada alumno paga un total de 65 euros");
+        }
 
+        else if (alumnos < 100 && alumnos >= 50 )
+        {
+            System.out.println("El pago a la agencia es: " + alumnos*70 + "" + " \n Cada alumno paga un total de 70 euros");
+        }
+        else if (alumnos < 50 && alumnos >= 30)
+        {
+            System.out.println("El pago a la agencia es: " + alumnos*95 + "" + " \n Cada alumno paga un total de 95 euros");
+        }
+        else if (alumnos < 30)
+        {
+            System.out.println("El pago a la agencia es: " + 4000 + " euros" + "" + " \n los alumnos no pagan nada");
+        }
 
     }
 }
---
+++ Ejercicio12.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+public class Ejercicio12 {
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        double precio;
+        double impuesto;
+        String tiempodia;
+        System.out.println("Digite el tiempo que duro la llamda (Usar numeros enteros)");
+        int tiempo = teclado.nextInt();
+        System.out.println("Que dia realizo la llamda ");
+        String dia = teclado.next() ; teclado.nextLine();
+
+        if (tiempo > 10){
+            precio = 3;
+        } else if (tiempo > 8){
+            precio = 2.5;
+        } else if (tiempo > 5){
+            precio = 1.80;
+        } else {
+            precio = 1;
+        }   
+        
+       
+        if (!dia.toUpperCase().equals("DOMINGO")) {
+          System.out.println("¿La llamada se realizó en horario de mañana o tarde? ");
+          tiempodia = teclado.nextLine();
+          if (tiempodia.toUpperCase().equals("MAÑANA")) {
+            impuesto = precio * 0.15;
+          } else if (tiempodia.toUpperCase().equals("TARDE")) {
+            impuesto = precio * 0.10;
+          
+          } else {
+            impuesto = 0; 
+          }
+        } else {
+          impuesto = precio * 0.03;
+        }
+        
+
+        if (impuesto == 0) {
+          System.out.println("El horario introducido es incorrecto, reinicia el programa "
+              + "e introduce los valores correctos.");
+        } else {
+          System.out.println("El precio total de la llamada es de " + (precio + impuesto) 
+              + " siendo el precio base de " + precio + " y el impuesto a pagar " + impuesto);
+        }
+   
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
